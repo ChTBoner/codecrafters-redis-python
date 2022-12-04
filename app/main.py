4e9c09d7e1ce4ff0baac33cfd54eb9fa039fedc6
Thibault Delattre <thibothekoala@yahoo.fr>
2022-12-04 11:47:01 +0100

    PONG OK

--- app/main.py
+++ app/main.py
@@ -9,8 +9,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_connection, _ = server_socket.accept() # wait for client
 
+    client_connection.recv(1024)
+    client_connection.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
